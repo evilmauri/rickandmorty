d940b19109a6242f339c01934cf5b5588d6004fd
Mauricio Gonzalez <58447379+evilmauri@users.noreply.github.com>
2023-09-27 15:31:38 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import './App.css';
+import Characters from './components/Characters';
+import imageRickMorty from './imgs/rick-morty.png';
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+  
+  const priApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character')
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results)
+
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 className="title">Rick & Morty</h1>
+      {characters ? (
+        <Characters characters= {characters} setCharacters={setCharacters}/>
+      ) : (
+        <>
+      <img src= {imageRickMorty} alt="Rick & Morty" className="img-home" />
+      <button onClick={priApi} className="btn-search">Buscar Personajes</button>
+      </>
+      )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
